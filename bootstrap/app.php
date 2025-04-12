13aa014745fb0f1928f2dfba45aa27b29b44a222
braxey <bradleyjohnson0009@gmail.com>
2025-04-11 23:51:38 -0400

    fix invalid signatures by updated trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            at: '*',
+        );
+
         $middleware->encryptCookies(except: ['appearance']);
 
         $middleware->web(append: [
